cab13937aefe5c5647ef0e1050d6afe1ac541e6a
sforseohn <sforseohn@gmail.com>
2023-01-30 02:05:00 +0900

    Pydantic 모델 코드 작성

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,31 +1,35 @@
 from typing import List, Union
 
 from pydantic import BaseModel
-
-class ItemBase(BaseModel):
-    title: str
-    description: Union[str, None] = None
-
-class ItemCreate(ItemBase):
+        
+class PlayerBase(BaseModel):
+    id: int
+    nickname: str 
+    room_id: int
+    
+class PlayerCreate(PlayerBase):
+    pass
+    
+class Player(PlayerBase):
+    cards: List[int] = []
+    
+    class Config:
+        orm_mode = True
+        
+class RoomBase(BaseModel): # 인스턴스를 생성할 때나 읽을 때 공통으로 필요한 속성
+    id: int
+    code: str
+      
+class RoomCreate(RoomBase): # 인스턴스를 생성할 때에만 필요하고 읽을 때는 필요 없는 속성
     pass
 
-class Item(ItemBase):
-    id: int
-    owner_id: int
+class Room(RoomBase): # 인스턴스를 읽을 때에만 표시되고 생성할 때에는 없어도 되는 속성
+    deck: List[int] = []
+    turninfo: int
+    
+    players: List[Player] = []
     
     class Config:
         orm_mode = True
 
-class UserBase(BaseModel):
-    email: str
-    
-class UserCreate(UserBase):
-    password: str
-    
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
-    
-    class Config:
-        orm_mode = True
+
