5031f94a5f4f69a1bf9712d14d6c10597154c5e0
sforseohn <sforseohn@gmail.com>
2023-01-29 23:08:54 +0900

    Database 구조 작성

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,8 +1,30 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from typing import List
 
 from .database import Base
 
+class Room(Base):
+    __tablename__ = "rooms"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, unique=True, index=True) # id와 code의 차이는?
+    deck = Column(List[int]) # ?
+    turninfo = Column(Integer) 
+    
+    players = relationship("Player", back_populates="room")
+
+class Player(Base):
+    __tablename__ = "players"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String, unique=True)
+    cards = Column(List[int]) # ?
+    room_id = Column(Integer, ForeignKey("rooms.id"))
+    
+    room = relationship("Room", back_populates="players")
+
+"""
 class User(Base):
     __tablename__ = "users"
     
@@ -22,3 +44,4 @@ class Item(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     
     owner = relationship("User", back_populates="items")
+"""
